d37db4ecc71ef297ae99fe1c814375930ec5ebe3
Luke <128195749+Luke-Galaxy@users.noreply.github.com>
2024-01-16 12:25:08 -0500

    Add files via upload

--- MkBdLuke.sh
+++ MkBdLuke.sh
@@ -57,7 +57,7 @@ function show_menu() {
 if ! command -v apktool &> /dev/null; then
     echo "APK Tool no está instalado en tu sistema."
     exit
-
+fi
 #Obtener el token de bot y el id chat de Telegram.
 cyan_color="\033[36m"
 reset_color="\033[0m"
@@ -71,7 +71,7 @@ echo -e "${cyan_color}¿Cuál es tu ID de chat de Telegram?${reset_color} ==> \c
 read chat_id
 
 #Navegacion del script para encontrar un apk.
-current_folder="$HOME/MkBdLuke/"
+current_folder="$HOME/MkBdLuke-/"
 
 while true; do
 
@@ -81,63 +81,68 @@ while true; do
     show_menu "$current_folder"
 
     if [ $? -eq 1 ]; then
-        next_folder=$(realpath "$current_folder../")
+        next_folder=$(realpath "$current_folder/..")
 
         # Comprobamos si el directorio es igual o superior a HOME
 
         if [[ "$next_folder" == "$HOME/MkBdLuke/"* ]]; then
             current_folder=$next_folder
         else
-            echo "$HOME/MkBdLuke"
+            echo "$HOME/MkBdLuke-/"
         fi
     else
         if [ -z "$apk_file" ]; then
-            echo "¿Deseas buscar nuevamente?"
-            select yn in "Sí" "No"; do
-                case $yn in
-                    Sí )
-                        break;;
-                    No )
-                        echo "error, ejecuta el script nuevamente"
-                        exit;;
-                esac
-            done
+            echo "Error: No se ha seleccionado un archivo APK."
+            continue
         else
-            break;
+            # Descompilar el apk encontrado.
+            apk_directory=$(dirname "$apk_file")
+            decompiled_apk="$apk_directory/decompiled"
+            apktool d "$apk_file" -o "$decompiled_apk"
+
+            if [ $? -eq 0 ]; then
+                # Insertar los cambios en el apk.
+                echo "$token" > "$decompiled_apk/assets/token.txt"
+                echo "$chat_id" > "$decompiled_apk/assets/id.txt"
+
+                # Compilar la APK modificada
+                cd "$decompiled_apk"
+                apktool empty-framework-dir --force
+                if apktool b . -o modified.apk; then
+                    echo "¡Compilación completada con éxito!"
+                    # Mover el APK compilado a la carpeta inicial
+                    mv modified.apk "$original_path/modified.apk"
+                else
+                    echo "Error al compilar la APK: $(cat apktool.err)"
+                    echo "¿Deseas intentar con otro archivo APK?"
+                    select yn in "Sí" "No"; do
+                        case $yn in
+                            Sí )
+                                continue 2;;
+                            No )
+                                echo "El script ha terminado."
+                                # Limpieza: eliminar la carpeta 'decompiled' si la compilación falla
+                                rm -rf "$decompiled_apk"
+                                exit;;
+                        esac
+                    done
+                fi
+                # Eliminar la carpeta decompiled si todo va bien
+                cd "$original_path"
+                rm -rf "$decompiled_apk"
+                break
+            else
+                echo "Error al descompilar el APK. ¿Deseas buscar nuevamente?"
+                select yn in "Sí" "No"; do
+                    case $yn in
+                        Sí )
+                            break;;
+                        No )
+                            echo "Error, ejecuta el script nuevamente"
+                            exit;;
+                    esac
+                done
+            fi
         fi
     fi
 done
-#Descompilar el apk encontrado.
-apk_directory=$(dirname "$apk_file")
-decompiled_apk="$apk_directory/decompiled"
-apktool d "$apk_file" -o "$decompiled_apk"
-
-#insertar los cambios en el apk.
-echo "$token" > "$decompiled_apk/assets/token.txt"
-echo "$chat_id" > "$decompiled_apk/assets/id.txt"
-
-#Compilar la APK modificada
-cd "$decompiled_apk"
-apktool empty-framework-dir --force
-if apktool b . -o modified.apk; then
-    echo "¡Compilación completada con éxito!"
-    #Mover el APK compilado a la carpeta inicial
-    mv modified.apk "$original_path/modified.apk"
-else
-    echo "Error al compilar la APK: $(cat apktool.err)"
-    echo "¿Deseas intentar con otro archivo APK?"
-    select yn in "Sí" "No"; do
-        case $yn in
-            Sí )
-                continue 2;;
-            No )
-                echo "El script ha terminado."
-                # Limpieza: eliminar la carpeta 'decompiled' si la compilación falla
-                rm -rf "$decompiled_apk"
-                exit;;
-        esac
-    done
-fi
-#Eliminar la carpeta decompiled si todo va bien
-cd "$original_path"
-rm -rf "$decompiled_apk"
